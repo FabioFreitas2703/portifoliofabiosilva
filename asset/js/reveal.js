504f7c9016c8e83f18611c28eaef63f4d270eee8
Fabio Silva <fabiosfreitas2703@gmail.com>
2025-06-07 22:16:27 -0300

    portifolio

---
+++ asset/js/reveal.js
@@ -0,0 +1,26 @@
+
+window.revelar = ScrollReveal({ reset:true })
+
+revelar.reveal('.intro-text',{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left'
+    
+})
+
+revelar.reveal('.intro-img',{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left'
+
+})
+
+revelar.reveal('.reavel',{
+    duration: 3000,
+    distance: '150px',
+    origin: 'left',
+    
+})
+
+
+
---
+++ asset/js/typed.js
@@ -0,0 +1,7 @@
+const typed = new Typed('#multiplay',{
+    strings: ['Desenvolvedor Front-end', 'desenvolvimento web',"Programação","freelancer"],
+    typeSpeed: 50,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
